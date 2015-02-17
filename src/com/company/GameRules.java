d2904478975eb4c8f63563af2bff168527920fdc
nrakochy <nick.rakochy@gmail.com>
2015-02-17 16:45:15 -0600

    Extracted FindWinningRow method from FindWinningRows with tests

--- src/com/company/GameRules.java
+++ src/com/company/GameRules.java
@@ -4,9 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameRules{
-    public GameRules(){}
 
-    public List<List> findWinningRows(Integer boardSize){
+    private Integer boardSize;
+    public GameRules(Integer boardSize){
+        this.boardSize = boardSize;
+    }
+
+    public List<List> findWinningRows(){
+        List<List> winningRowCombos = new ArrayList<List>();
+        Integer spaceNum = 1;
+        while(winningRowCombos.size() < boardSize){
+            List<Integer> winner = findWinningRow(spaceNum, boardSize);
+            winningRowCombos.add(winner);
+            spaceNum = winner.get(boardSize - 1) + 1;
+        }
+        return winningRowCombos;
+    }
+
+    public List<Integer> findWinningRow(Integer spaceNum, Integer rowLength) {
+        List<Integer> winningRow = new ArrayList<Integer>();
+        while (winningRow.size() < rowLength) {
+            winningRow.add(spaceNum);
+            spaceNum++;
+        }
+        return winningRow;
+    }
+
+    public List<List> findWinningColumns(){
         List<List> winningRowCombos = new ArrayList<List>();
         Integer spaceNum = 1;
         while(winningRowCombos.size() < boardSize){
@@ -19,9 +43,5 @@ public class GameRules{
         }
         return winningRowCombos;
     }
-
-
-
-
 
 }
--- test/com/company/GameRulesTest.java
+++ test/com/company/GameRulesTest.java
@@ -11,7 +11,8 @@ import static org.junit.Assert.*;
 public class GameRulesTest {
     Integer boardSize3 = 3;
     Integer boardSize4 = 4;
-    public GameRules rules = new GameRules();
+    public GameRules rules3x3 = new GameRules(boardSize3);
+    public GameRules rules4x4 = new GameRules(boardSize4);
 
     @Test
     public void correctlyReturnsListOfAllWinningRowCombos3x3() throws Exception {
@@ -22,7 +23,7 @@ public class GameRulesTest {
         winningRowCombos.add(winningRow1);
         winningRowCombos.add(winningRow2);
         winningRowCombos.add(winningRow3);
-        assertEquals("Did not return 9 open spaces", winningRowCombos, rules.findWinningRows(boardSize3));
+        assertEquals("Did not return 9 open spaces", winningRowCombos, rules3x3.findWinningRows());
     }
 
     @Test
@@ -36,6 +37,22 @@ public class GameRulesTest {
         winningRowCombos.add(winningRow2);
         winningRowCombos.add(winningRow3);
         winningRowCombos.add(winningRow4);
-        assertEquals("Did not return 9 open spaces", winningRowCombos, rules.findWinningRows(boardSize4));
+        assertEquals("Did not return 9 open spaces", winningRowCombos, rules4x4.findWinningRows());
+    }
+
+    @Test
+    public void correctlyReturnsListOfWinningCombosForSingleRow3x3() throws Exception {
+        Integer spaceNum = 1;
+        Integer rowLength = 3;
+        List<Integer> winningRow1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3));
+        assertEquals("Did not return winning row combo for 3x3", winningRow1, rules3x3.findWinningRow(spaceNum, rowLength));
+    }
+
+    @Test
+    public void correctlyReturnsListOfWinningCombosForSingleRow4x4() throws Exception {
+        Integer spaceNum = 1;
+        Integer rowLength = 4;
+        List<Integer> winningRow1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4));
+        assertEquals("Did not return winning row combo for 4x4", winningRow1, rules3x3.findWinningRow(spaceNum, rowLength));
     }
 }
