b71a18492d58218658524b9ac902f86c7cde01f7
nrakochy <nick.rakochy@gmail.com>
2015-02-17 14:50:40 -0600

    Board class finds open spaces which have not been played yet

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,29 +1,45 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-/**
- * Created by user on 1/23/15.
- */
 public class Board {
 
     public Map<Integer, String> boardSpaces;
+    private String emptySpace = "";
 
     public Board(int boardSize) {
         boardSpaces = createBoard(boardSize);
     }
 
 
-    public Map createBoard(int boardSize) {
-        Map emptySpaces = new HashMap<Integer, String>();
-        String emptySpace = "";
+    public Map<Integer, String> createBoard(int boardSize) {
+        Map<Integer, String> emptySpaces = new HashMap<Integer, String>();
         int numBoardSpaces = boardSize * boardSize;
         int spaceNum = 1;
-        while(spaceNum <= numBoardSpaces){
+        while (spaceNum <= numBoardSpaces) {
             emptySpaces.put(spaceNum, emptySpace);
             ++spaceNum;
         }
         return emptySpaces;
-        }
     }
+
+    public int getBoardSize() {
+        return boardSpaces.size();
+    }
+
+    public List<Integer> findOpenSpaces(){
+        List<Integer> openSpaces = new ArrayList();
+        for (Map.Entry<Integer, String> space : boardSpaces.entrySet()){
+            String spaceValue = space.getValue();
+            if(spaceValue == emptySpace){
+                openSpaces.add(space.getKey());
+            }
+        }
+        return openSpaces;
+    }
+
+}
+
--- test/com/company/BoardTest.java
+++ test/com/company/BoardTest.java
@@ -1,6 +1,11 @@
 package com.company;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static junit.framework.TestCase.assertEquals;
 
 
@@ -12,12 +17,37 @@ public class BoardTest {
 
 
     @Test
-    public void verify3x3BoardCreationRendersHashMapWithLengthOf9() {
+    public void verify3x3BoardCreationRendersHashMapWithLengthOf9() throws Exception {
         assertEquals("Did not create 3x3 board", 9, board3.boardSpaces.size());
     }
 
     @Test
-    public void verify4x4BoardCreationRendersHashMapWithLengthOf16() {
+    public void verify4x4BoardCreationRendersHashMapWithLengthOf16() throws Exception {
         assertEquals("Did not create 3x3 board", 16, board4.boardSpaces.size());
+    }
+
+    @Test
+    public void verifyCorrectBoardSpacesReturnedOn3x3() throws Exception {
+        assertEquals("Did not return 9 for board with 9 spaces", 9, board3.getBoardSize());
+    }
+
+    @Test
+    public void verifyCorrectBoardSpacesReturnedOn4x4() throws Exception {
+        assertEquals("Did not return 9 for board with 16 spaces", 16, board4.getBoardSize());
+    }
+
+    @Test
+    public void correctlyFindsSpacesOnTheBoardWhichHaveNotBeenPlayedYet3x3() throws Exception {
+        List<Integer> openBoard = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        assertEquals("Did not return 9 open spaces", openBoard, board3.findOpenSpaces());
+
+    }
+
+    @Test
+    public void correctlyFindsSpacesOnTheBoardWhichHaveNotBeenPlayedYet4x4() throws Exception {
+        List<Integer> openBoard =
+                new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16));
+        assertEquals("Did not return 16 open spaces", openBoard, board4.findOpenSpaces());
+
     }
 }
